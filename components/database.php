fe73e2c1ebc011db524be4675f89618de7e05197
Finkfo <valentin.valleepro@gmail.com>
2022-12-01 11:57:25 +0100

    index/card

--- components/database.php
+++ components/database.php
@@ -5,7 +5,7 @@ $_SESSION;
 $bdd = new PDO(
     'mysql:host=localhost;dbname=grpc_grv;charset=utf8',
     'root',
-    'root'
+    'root1'
 );
 $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
 ?>
